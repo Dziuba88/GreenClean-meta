9b6b472ba792eb8b84d02011e062613abedc2445
Dziuba88 <Dziuba88@Gmail.com>
2018-07-13 19:44:14 +0300

    all pages

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -68,5 +68,147 @@ $( document ).ready(function() {
   $('.footer--menu__toggle').click(function () {
     $(this).parent('.footer--menu').toggleClass('active')
   });
+
+  $(document).on('click','.video--play',function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var wrapper = $this.closest('.video');
+
+    $('.video').removeClass('active');
+    $('.video').find('.video--iframe').attr('src', '');
+    videoPlay(wrapper);
+  });
+  function videoPlay(wrapper) {
+    var iframe = wrapper.find('.video--iframe');
+    var src = iframe.data('src');
+    wrapper.addClass('active');
+    iframe.attr('src',src);
+  };
+
+  $('.popup--image').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    image: {
+      verticalFit: false,
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>'+item.el.attr('data-addr')+'</small>';
+      }
+    }
+  });
+  $('.portfolio--list').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>'+item.el.attr('data-addr')+'</small>';
+      }
+    }
+  });
 });
 
+  // google map //
+  if ($('#g-map').length) {
+    var map_styler = [
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ];
+    var locations = [
+      ["Marker Title", 53.3244431, -6.3857856, "img/marker.png"],
+      ["Marker Title", 53.3344431, -6.3257856, "img/marker.png"],
+      ["Marker Title", 53.3944431, -6.2557856, "img/marker.png"],
+      ["Marker Title", 53.3144431, -6.2557856, "img/marker.png"],
+      ["Marker Title", 53.3044431, -6.2057856, "img/marker.png"],
+      ["Marker Title", 53.36044431, -6.2457856, "img/marker.png"],
+    ];
+    var map = new google.maps.Map(document.getElementById('g-map'), {
+        zoom: 11,
+        styles: map_styler,
+        center: new google.maps.LatLng(53.3244431, -6.2557856),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+            icon: {
+                url: locations[i][3],
+                scaledSize: new google.maps.Size(72, 82)
+            }
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+  }
--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,3 +1,4 @@
 //@prepros-append plugins/owl.carousel.js
 //@prepros-append plugins/custom.select.js
 //@prepros-append plugins/dropdown.js
+//@prepros-append plugins/magnific-popup.js
